2624aaa6e465c2fafda2e9a5ab844957cde03d65
EarlyBird5578 <EarlyBird-Service@hotmail.com>
2025-07-29 12:05:27 +0200

    Add files via upload

---
+++ styles/themes/natur.config.ts
@@ -0,0 +1,11 @@
+
+export const naturConfig = {
+  background: "bg-green-100",
+  textColor: "text-green-900",
+  buttons: [
+    { label: "Frequenz Welt", icon: "🌍", path: "/frequency" },
+    { label: "Frequenz Umgebung", icon: "🏡", path: "/frequency-local" },
+    { label: "Amplitude", icon: "📡", path: "/amplitude" },
+    { label: "Trägheit", icon: "🌱", path: "/profile" },
+  ],
+};
